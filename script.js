2508445a13cb0d4942fb44233096d12dfd8d4c28
Giuseppe <giusepperecupero97@gmail.com>
2025-05-28 17:04:00 +0200

    THE FUN UPDATE

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-function scrollToElement(elementSelector, instance = 0) {
-    const elements = document.querySelectorAll(elementSelector);
-    if (elements.length > instance) {
-        elements[instance].scrollIntoView({behavior: 'smooth'});
+function toggleSection(sectionId, logicType) {
+    const input1 = document.getElementById(logicType.toLowerCase() + '1').checked;
+    const input2 = document.getElementById(logicType.toLowerCase() + '2').checked;
+
+    let show = false;
+    switch (logicType) {
+        case 'AND':
+            show = input1 && input2;
+            break;
+        case 'OR':
+            show = input1 || input2;
+            break;
+        case 'NAND':
+            show = !(input1 && input2);
+            break;
+    }
+
+    const section = document.getElementById(sectionId);
+    if (show) {
+        section.classList.remove('hidden');
+    } else {
+        section.classList.add('hidden');
     }
 }
-
-const link1 = document.getElementById("link1");
-const link2 = document.getElementById("link2");
-const link3 = document.getElementById("link3");
-
-link1.addEventListener('click', () => {
-    scrollToElement('.header');
-});
-
-link2.addEventListener('click', () => {
-    // Scroll to the second element with "header" class
-    scrollToElement('.header', 1);
-});
-
-link3.addEventListener('click', () => {
-    scrollToElement('.column');
-});
